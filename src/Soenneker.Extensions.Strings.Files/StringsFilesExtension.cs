12aac97586dcdee6d20ecb9e1287d5aa452ba261
Jake Soenneker <jake@soenneker.com>
2026-08-25 14:26:35 -0500

    Improves performance and allocation

--- src/Soenneker.Extensions.Strings.Files/StringsFilesExtension.cs
+++ src/Soenneker.Extensions.Strings.Files/StringsFilesExtension.cs
@@ -1,10 +1,9 @@
 ﻿using Soenneker.Extensions.DateTime;
-using Soenneker.Extensions.DateTimeOffsets;
-using Soenneker.Extensions.String;
 using Soenneker.Utils.PooledStringBuilders;
 using Soenneker.Utils.TimeZones;
 using System;
 using System.Diagnostics.Contracts;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 
 namespace Soenneker.Extensions.Strings.Files;
@@ -23,7 +22,10 @@ public static class StringsFilesExtension
         utcNow ??= System.DateTime.UtcNow;
         timeZoneInfo ??= Tz.Eastern;
 
-        return AppendCore(value, utcNow.Value.ToTzFileName(timeZoneInfo));
+        System.DateTime converted = utcNow.Value.ToTz(timeZoneInfo);
+        Span<char> timestamp = stackalloc char[19];
+        converted.TryFormat(timestamp, out int written, "yyyy-MM-dd--HH-mm-ss", CultureInfo.InvariantCulture);
+        return AppendCore(value, timestamp[..written]);
     }
 
     /// <summary>
@@ -35,23 +37,31 @@ public static class StringsFilesExtension
         utcNow ??= DateTimeOffset.UtcNow;
         timeZoneInfo ??= Tz.Eastern;
 
-        return AppendCore(value, utcNow.Value.ToTzFileName(timeZoneInfo));
+        DateTimeOffset converted = TimeZoneInfo.ConvertTime(utcNow.Value, timeZoneInfo);
+        Span<char> timestamp = stackalloc char[19];
+        converted.TryFormat(timestamp, out int written, "yyyy-MM-dd--HH-mm-ss", CultureInfo.InvariantCulture);
+        return AppendCore(value, timestamp[..written]);
     }
 
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static string AppendCore(string? value, string timestamp)
+    private static string AppendCore(string? value, ReadOnlySpan<char> timestamp)
     {
         if (value == null)
-            return timestamp;
+            return timestamp.ToString();
 
-        using var sb = new PooledStringBuilder(64);
-
-        sb.Append(value.RemoveWhiteSpace());
-        sb.Append('-');
-        sb.Append(timestamp);
-
-        return sb.ToString();
+        var sb = new PooledStringBuilder(value.Length + timestamp.Length + 1);
+        try
+        {
+            AppendWithoutWhiteSpace(value, ref sb);
+            sb.Append('-');
+            sb.Append(timestamp);
+            return sb.ToString();
+        }
+        finally
+        {
+            sb.Dispose();
+        }
     }
 
     /// <summary>
@@ -60,14 +70,54 @@ public static class StringsFilesExtension
     [Pure]
     public static string ToFileName(this string? value, string extension, TimeZoneInfo? timeZoneInfo = null)
     {
-        using var sb = new PooledStringBuilder(96);
+        timeZoneInfo ??= Tz.Eastern;
+        System.DateTime converted = System.DateTime.UtcNow.ToTz(timeZoneInfo);
+        Span<char> timestamp = stackalloc char[19];
+        converted.TryFormat(timestamp, out int written, "yyyy-MM-dd--HH-mm-ss", CultureInfo.InvariantCulture);
 
-        sb.Append(value.AppendDateTime(timeZoneInfo));
-        sb.Append('-');
-        sb.Append(Guid.NewGuid());
-        sb.Append('.');
-        sb.Append(extension);
+        int capacity = (value?.Length ?? 0) + written + extension.Length + 39;
+        var sb = new PooledStringBuilder(capacity);
+        try
+        {
+            if (value is not null)
+            {
+                AppendWithoutWhiteSpace(value, ref sb);
+                sb.Append('-');
+            }
 
-        return sb.ToString();
+            for (var i = 0; i < written; i++)
+                sb.Append(timestamp[i]);
+
+            sb.Append('-');
+            sb.Append(Guid.NewGuid());
+            sb.Append('.');
+            sb.Append(extension);
+
+            return sb.ToString();
+        }
+        finally
+        {
+            sb.Dispose();
+        }
+    }
+
+    private static void AppendWithoutWhiteSpace(string value, ref PooledStringBuilder builder)
+    {
+        ReadOnlySpan<char> span = value;
+        var segmentStart = 0;
+
+        for (var i = 0; i < span.Length; i++)
+        {
+            if (!char.IsWhiteSpace(span[i]))
+                continue;
+
+            if (i > segmentStart)
+                builder.Append(span[segmentStart..i]);
+
+            segmentStart = i + 1;
+        }
+
+        if (segmentStart < span.Length)
+            builder.Append(span[segmentStart..]);
     }
 }
